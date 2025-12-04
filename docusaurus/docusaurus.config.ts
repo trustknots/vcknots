a8971285dc08d53d943f236008db3911a38d6c13
Kazunari FUJITA <kazunari.fujita@ctc-g.co.jp>
2025-12-04 13:53:10 +0900

    chore:run biome format

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -5,10 +5,9 @@ import { themes as prismThemes } from 'prism-react-renderer'
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: "trustknots/vcknots",
-  tagline:
-    "Pluggable framework for building Verifiable Credentials ecosystems.",
-  favicon: "img/favicon.ico",
+  title: 'trustknots/vcknots',
+  tagline: 'Pluggable framework for building Verifiable Credentials ecosystems.',
+  favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -16,48 +15,48 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://trustknots.github.io",
+  url: 'https://trustknots.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/vcknots/",
+  baseUrl: '/vcknots/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "trustknots", // Usually your GitHub org/user name.
-  projectName: "vcknots", // Usually your repo name.
+  organizationName: 'trustknots', // Usually your GitHub org/user name.
+  projectName: 'vcknots', // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en", "ja"],
+    defaultLocale: 'en',
+    locales: ['en', 'ja'],
     localeConfigs: {
       en: {
-        label: "English",
-        htmlLang: "en",
+        label: 'English',
+        htmlLang: 'en',
       },
       ja: {
-        label: "日本語",
-        htmlLang: "ja",
+        label: '日本語',
+        htmlLang: 'ja',
       },
     },
-    path: "../docs/i18n",
+    path: '../docs/i18n',
   },
 
   presets: [
     [
-      "classic",
+      'classic',
       {
         docs: {
-          path: "../docs/en",
-          sidebarPath: "./sidebars.ts",
+          path: '../docs/en',
+          sidebarPath: './sidebars.ts',
         },
         blog: false,
         theme: {
-          customCss: "./src/css/custom.css",
+          customCss: './src/css/custom.css',
         },
       } satisfies Preset.Options,
     ],
@@ -65,48 +64,48 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: 'img/docusaurus-social-card.jpg',
     colorMode: {
       respectPrefersColorScheme: true,
     },
     docs: { sidebar: { autoCollapseCategories: true } },
     navbar: {
-      title: "trustknots/vcknots",
+      title: 'trustknots/vcknots',
       logo: {
-        alt: "My Site Logo",
-        src: "img/trustknots.png",
+        alt: 'My Site Logo',
+        src: 'img/trustknots.png',
       },
       items: [
         {
-          type: "docSidebar",
-          sidebarId: "tutorialSidebar",
-          position: "left",
-          label: "Docs",
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Docs',
         },
         {
-          type: "localeDropdown",
-          position: "right",
+          type: 'localeDropdown',
+          position: 'right',
         },
       ],
     },
     footer: {
-      style: "dark",
+      style: 'dark',
       links: [
         {
-          title: "Docs",
+          title: 'Docs',
           items: [
             {
-              label: "vcknots",
-              to: "/docs/issuer",
+              label: 'vcknots',
+              to: '/docs/issuer',
             },
           ],
         },
         {
-          title: "More",
+          title: 'More',
           items: [
             {
-              label: "GitHub",
-              href: "https://github.com/trustknots/vcknots",
+              label: 'GitHub',
+              href: 'https://github.com/trustknots/vcknots',
             },
           ],
         },
@@ -118,6 +117,6 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
-};
+}
 
 export default config
