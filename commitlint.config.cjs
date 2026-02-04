8cd95a97ad4afbb68f960ecec9d5f80e69b8b5c8
yaaasuu <anyongw01123@gmail.com>
2026-02-04 09:55:46 +0900

    chore: delete unused rules and file

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,21 +1,3 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'header-max-length': [2, 'always', 100],
-        'header-trim': [2, 'always'],
-        'subject-empty': [2, 'never'],
-        'subject-full-stop': [2, 'never', '.'],
-        'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
-        'type-empty': [2, 'never'],
-        'type-case': [2, 'always', 'lower-case'],
-        'type-enum': [
-            2,
-            'always',
-            ['build','chore','ci','docs','feat','fix','perf','refactor','revert','style','test']
-        ],
-        'body-leading-blank': [2, 'always'],
-        'body-max-line-length': [2, 'always', 100],
-        'footer-leading-blank': [2, 'always'],
-        'footer-max-line-length': [2, 'always', 100]
-    },
+  extends: ['@commitlint/config-conventional'],
 }
