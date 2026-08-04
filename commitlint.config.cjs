4c3260950099ad691cba297142895931f456b5c3
h.hirai <hiroyuki.hirai.022@ctc-g.co.jp>
2026-08-04 13:17:23 +0900

    fix: support breaking change ! notation in wagoid action

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,10 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(\w*)(?:\(([\w$.\-* ]*)\))?!?: (.+)$/,
+      breakingHeaderPattern: /^(\w*)(?:\(([\w$.\-* ]*)\))?!: (.+)$/,
+      headerCorrespondence: ['type', 'scope', 'subject'],
+    },
+  },
 }
